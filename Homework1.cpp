32b69d7703d9f3310413fd0d66b37daf7ec5624b
DanilChery <52321592+DanilChery@users.noreply.github.com>
2020-11-20 14:15:44 +0300

    Add files via upload

---
+++ Homework1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    string snums;
+    cin >> snums;
+   // int nums = stoi(snums);
+   // cout << nums;
+    cout << snums.length();
+
+}
+
